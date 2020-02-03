ef72c5aea35a711d9113e9771d82c484a85b0291
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-03 20:15:12 +0100

    object oriented programming introduction

---
+++ ObjectOrientedProgrammingIntroduction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Person a = new Person();
+        Console.WriteLine(a.getName());
+
+        a.setName("My name is Alex");
+        Console.WriteLine(a.getName());
+    }
+}
+
+class Person
+{
+    private static string name = "My name is Max";
+    public string getName()
+    {
+        return name;
+    }
+    public void setName(string newName)
+    {
+        name = newName;
+    }
+}
