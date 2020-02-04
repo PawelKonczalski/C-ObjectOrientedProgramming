0a22f1925fe5812cebf38228d9316c9a27c2d229
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-04 20:55:06 +0100

    nested classes

---
+++ NestedClasses/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car();
+        Console.WriteLine(car.enginePower());
+    }
+}
+
+class Car
+{
+    private class Engine
+    {
+        public Engine(uint power = 10)
+        {
+            this.power = power;
+            Console.WriteLine("Engine");
+        }
+        public uint power;
+    }
+    public Car()
+    {
+        engine = new Engine();
+        Console.WriteLine("Car");
+    }
+
+    public uint enginePower()
+    {
+        return this.engine.power;
+    }
+    
+    private Engine engine;
+}
