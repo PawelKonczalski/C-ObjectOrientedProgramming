168f6de6bcfe7c931ca43f828236e6cf3637a214
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-12 15:21:31 +0100

    sealed classes and methods

---
+++ SealedClassesAndMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+
+class EndClass
+{
+    public virtual void test()
+    {
+        
+    }
+}
+
+class DerivativeClass : EndClass
+{
+    public sealed override void test()
+    {
+        base.test();
+    }
+}
