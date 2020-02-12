8cb8f798967c4957afef9c64ab8af8a3b25287f4
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-12 15:12:17 +0100

    virtual and override

---
+++ VirtualAndOverride/Point.cs
@@ -0,0 +1,22 @@
+﻿public class Point
+{
+    public int x { get; set; }
+    public int y { get; set; }
+
+    public Point()
+    {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    virtual public string getPoint()
+    {
+        return this.x + ", " + this.y;
+    }
+}
---
+++ VirtualAndOverride/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PolymorphismObjectClassIsOperatorAsAndCasting
+{
+    class Program
+    {
+        static void Display(Point p)
+        {
+            // if(p is Point3D)
+            // Console.WriteLine(((Point3D)p).getPoint());
+            // else
+            //     Console.WriteLine(p.getPoint());
+            
+            Point3D p3d = p as Point3D;
+            if (p3d == null)
+            {
+                Console.WriteLine(p.getPoint());
+            }
+            else
+            {
+                Console.WriteLine(p3d.getPoint());
+            }
+        }
+
+        static string getClassName(Object x)
+        {
+            return x.ToString();
+        }
+        
+        static void Main(string[] args)
+        {
+            Point a;
+            Point b;
+            Point3D c = new Point3D(50,100,150);
+            
+            a = new Point(1,5);
+            Console.WriteLine(a.getPoint());
+            
+            b = new Point3D(5,10,15);
+            Console.WriteLine(((Point3D)b).getPoint());
+            
+            
+            Display(a);
+            Display(b);
+            Display(c);
+            
+            Console.WriteLine(getClassName(a));
+            Console.WriteLine(getClassName(b));
+            Console.WriteLine(getClassName(c));
+            
+            Console.WriteLine(b.getPoint());
+        }
+    }
+}
