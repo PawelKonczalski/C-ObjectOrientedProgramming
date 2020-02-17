9302f97f3cc5a682fb6918bb8063a340c7214411
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-17 16:16:01 +0100

    enum

---
+++ Enum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    public enum TimeOfDay
+    {
+        Morning,
+        Afternoon,
+        Evening
+    }
+
+    public static string getGreetings(TimeOfDay time)
+    {
+        switch (time)
+        {
+            case TimeOfDay.Morning: return "Good morning";
+            case TimeOfDay.Afternoon: return "Good afternoon";
+            case TimeOfDay.Evening: return "Good evening";
+            default: return "Something went wrong";
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        TimeOfDay a = TimeOfDay.Morning;
+        Console.WriteLine(getGreetings(a));
+    }
+}
