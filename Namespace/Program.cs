2e71e0bcc2ecd8d8649b70b84acefd7211a30f15
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-17 16:48:38 +0100

    namespace

---
+++ Namespace/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Namespace
+{
+    class Program
+    {
+        public static double PI = 3.14;
+        public Program()
+        {
+            Console.WriteLine("I'm constructor from namespace 'Namespace'");
+        }
+        static void Main(string[] args)
+        {
+            global::Program zm = new global::Program();
+            // MyNamespace.Program zm = new MyNamespace.Program();
+            // Program nazwa_zm = new Program();
+            Console.WriteLine("Test");
+            Console.ReadLine();
+        }
+    }
+}
+
+// namespace MyNamespace
+// {
+    class Program
+    {
+        private double c = Namespace.Program.PI;
+        public Program()
+        {
+            Console.WriteLine("I'm constructor from global namespace " + c);
+        }
+    }
+// }
