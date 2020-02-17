6071f617d2c57cea744b511b0a9c17662128904a
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-17 19:44:30 +0100

    generic methods

---
+++ GenericMethods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void Swap<T>(ref T a, ref T b)
+    {
+        T tmp;
+
+        tmp = a;
+        a = b;
+        b = tmp;
+    }
+    
+    static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 20;
+
+        string c = "lala";
+        string d = "haha";
+        
+        Swap(ref a, ref b);
+        Swap(ref c, ref d);
+
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+        Console.WriteLine("c = " + c);
+        Console.WriteLine("d = " + d);
+        Console.ReadLine();
+    }
+}
