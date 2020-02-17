d11001a251bede24f7498100009a8a1b220e3e23
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-17 18:15:46 +0100

    indeksery

---
+++ Indeksery/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Sentence sentence = new Sentence("this is test message");
+        Console.WriteLine(sentence[1]);
+        Console.ReadLine();
+    }
+}
+
+class Sentence
+{
+    private string[] words;
+
+    public Sentence(string sentence)
+    {
+        this.words = sentence.Split();
+    }
+
+    public string this[int n]
+    {
+        get
+        {
+            if (words.Length > n)
+                return words[n];
+            else
+                return null;
+        }
+        set
+        {
+            if (words.Length > n)
+                words[n] = value;
+        }
+    }
+}
