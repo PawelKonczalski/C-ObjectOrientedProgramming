7fb2b9698dfcff1d9a11c0135e6f610cc9abd74d
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-20 16:35:30 +0100

    extending methods

---
+++ ExtendingMethods/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string a = "Any sentence for method testing";
+        // Console.WriteLine(StringHelper.IsCapitalized(a));
+        Console.WriteLine(a.IsCapitalized());
+        
+       Test b = new Test();
+       b.tester();
+    }
+}
+
+static class StringHelper
+{
+    // public static bool IsCapitalized(string value)
+    // {
+    //     return Char.IsUpper(value[0]);
+    // }
+    public static bool IsCapitalized(this string value)
+    {
+        return Char.IsUpper(value[0]);
+    }
+}
+
+class Test
+{
+}
+
+class Test2 : Test
+{
+}
+
+static class TestHelper
+{
+    public static bool tester(this Test value)
+    {
+        return true;
+    }
+}
