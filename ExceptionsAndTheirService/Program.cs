713bff5c6f79ee38891e4695d0c94b400accd3b0
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-20 17:05:24 +0100

    exceptions and their service

---
+++ ExceptionsAndTheirService/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 0;
+        string c;
+
+        c = Console.ReadLine();
+
+        try
+        {
+            // Console.WriteLine(a / b);
+            if (a == 10)
+            {
+                throw new OurNameException("and was equal to 10");
+            }
+            int tmp = Convert.ToInt32(c);
+            Console.WriteLine(a / tmp);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            // throw;
+        }
+        finally
+        {
+            Console.WriteLine("message at the end of the program " +
+                              "regardless of whether an exception " +
+                              "occurs");
+        }
+    }
+}
+
+class OurNameException : Exception
+{
+    public OurNameException(string? message) : base(message)
+    {
+    }
+}
