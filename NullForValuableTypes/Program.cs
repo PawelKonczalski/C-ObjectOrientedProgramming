14e70756fa657bcf5bad43adcbaf575ce4f27af3
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2020-02-20 16:13:27 +0100

    null for valuable types

---
+++ NullForValuableTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    public static int? getSomethingFromDataBase()
+    {
+        return null;
+    }
+
+    static void Main(string[] args)
+    {
+        int? a = getSomethingFromDataBase();
+
+        Console.WriteLine(a);
+
+        if (a == null)
+        {
+            Console.WriteLine("you didn't enter a value");
+        }
+        else
+        {
+            Console.WriteLine(a);
+        }
+
+        int b = a ?? 0;
+        Console.WriteLine(b);
+    }
+}
